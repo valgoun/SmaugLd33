17a5b403adf605fdf3a30a70f3ec5a0b39614d27
valgoun <valgoun@gmail.com>
2015-08-22 09:18:12 +0200

    UnityProject

---
+++ SmaugLd33/SmaugLd33/Assets/Scripts/Spawnner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawnner : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject
+		Ball;
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Ray r = Camera.main.ScreenPointToRay (Input.mousePosition);
+	}
+}
